f8d4c4491d5ab711e1ee4f3a800d805cc88fd24c
Mudasir Fayaz <mudasirfayazitoo@gmail.com>
2024-10-18 18:58:57 +0530

    Initail Build

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,6 @@ import { useTheme } from "next-themes";
 
 export default function Header() {
     const {theme, setTheme} = useTheme()
-    setTheme('light');
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
